231443c4b870bfd8fd895a674d31fb4a8efcf4e1
vlad <vlad@EPBYMINW4476t1.minsk.epam.com>
2018-03-21 22:54:49 +0300

    add vars to main

--- terraform/global/vpc/vars.tf
+++ terraform/global/vpc/vars.tf
@@ -2,3 +2,8 @@ variable "region" {
   description = "The aws region (eu-central-1)"
   default = "eu-central-1"
 }
+
+variable "env" {
+  description = "The env type (prod/stage/etc)"
+  default = "dev"
+}
